047f58bd6f8e434a65e9991bf47dda6f16d65a43
Aaron Panchal <aaron.panchal@appfolio.com>
2015-07-17 16:48:10 -0700

    ap - clean up for github page

--- app/scripts/main.js
+++
@@ -1,7 +0,0 @@
-import 'babel/polyfill';
-
-import React from 'react';
-import WarGame from './war_game';
-import Game from './models/game';
-
-React.render(<WarGame />, window.document.querySelector("#game_target"));
